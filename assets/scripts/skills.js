a7bb1de2f417440250ca1801abe6604671c462f7
Conkeegs <conkeegs@gmail.com>
2021-06-24 15:40:28 -0400

    finished functionality for pie charts on 'skills.html'

--- assets/scripts/skills.js
+++ assets/scripts/skills.js
@@ -13,27 +13,51 @@ for (let skill of skills) {
 
 }
 
+/**
+ * Rotates the pie chart divs based on the skill percent value inside of the 'skill' parameter.
+ * 
+ * @param {element} skill An element with class attribute 'skills-middle-container-background-skill'.
+ */
 function rotateSkill(skill) {
 
+    const rotateMultiplier = 3.6;
     let spinnerPercentage = parseInt(skill.children[0].textContent.trim());
-    let rotateDegree = 3.6 * spinnerPercentage;
+    let rotateDegree = rotateMultiplier * spinnerPercentage;
+
     let spinnerLeft = skill.children[1];
     let spinnerRight = skill.children[2];
     let spinnerCover = skill.children[3];
 
-     if (spinnerPercentage <= 50) {
+    if (spinnerPercentage <= 50) {
+
+        spinnerLeft.style.transitionTimingFunction = "ease-in-out";
 
         spinnerRight.style.display = "none";
-        spinnerLeft.style.transform = `rotate(${rotateDegree}deg)`;
+        spinnerLeft.style.transition = "500ms";
+        setTimeout(() => spinnerLeft.style.transform = `rotate(${rotateDegree}deg)`, 10);
 
-     }
-     else {
+    } else {
 
-        spinnerLeft.style.transform = "rotate(180deg)";
-        spinnerCover.style.display = "none";
-        spinnerRight.style.display = "block";
-        spinnerRight.style.transform = `rotate(${rotateDegree - 180}deg)`;
+        spinnerRight.style.opacity = "0.0";
+        spinnerRight.style.transitionProperty = "opacity, transform";
+        spinnerRight.style.transitionDuration = "0s, 250ms";
+        spinnerLeft.style.transition = "250ms";
 
-     }
+        spinnerLeft.style.transitionTimingFunction = "ease-in";
+
+        setTimeout(function() {
+
+            spinnerLeft.style.transform = "rotate(180deg)";
+            setTimeout(function() {
+
+                spinnerRight.style.opacity = "1.0";
+                spinnerRight.style.transform = `rotate(${rotateDegree - 180}deg)`;
+
+            }, 250);
+
+
+        }, 10);
+
+    }
 
 }
