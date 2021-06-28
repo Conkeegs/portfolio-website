071430c54a84865d8d80eb59d5d01b8ee3619784
Conkeegs <conkeegs@gmail.com>
2021-06-28 16:41:20 -0400

    finished percentages and tint functionality for pie charts on 'skills.html'

--- assets/scripts/skills.js
+++ assets/scripts/skills.js
@@ -5,57 +5,61 @@ Scripts for 'skills.html'
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 */
-let skills = document.getElementsByClassName("skills-middle-container-background-skill");
+let skills = document.getElementsByClassName(
+    'skills-middle-container-background-skill'
+);
 
 for (let skill of skills) {
-
     rotateSkill(skill);
-
 }
 
 /**
  * Rotates the pie chart divs based on the skill percent value inside of the 'skill' parameter.
- * 
+ *
  * @param {element} skill An element with class attribute 'skills-middle-container-background-skill'.
  */
 function rotateSkill(skill) {
-
     const rotateMultiplier = 3.6;
-    let spinnerPercentage = parseInt(skill.children[1].textContent.trim().replace("%", ""));
+    let spinnerPercentage = parseInt(
+        skill.children[1].textContent.trim().replace('%', '')
+    );
     let rotateDegree = rotateMultiplier * spinnerPercentage;
 
     let skillCover = skill.children[0];
+    let skillPercentage = skill.children[1];
     let spinnerLeft = skill.children[2];
     let spinnerRight = skill.children[3];
 
     if (spinnerPercentage <= 50) {
-        spinnerLeft.style.transitionTimingFunction = "ease-in-out";
+        spinnerLeft.style.transitionTimingFunction = 'ease-in-out';
+        spinnerRight.style.display = 'none';
+        spinnerLeft.style.transition = '500ms';
 
-        spinnerRight.style.display = "none";
-        spinnerLeft.style.transition = "500ms";
         setTimeout(function() {
-
             spinnerLeft.style.transform = `rotate(${rotateDegree}deg)`;
-            setTimeout(() => skillCover.style.opacity = "0.4", 500);
-
+            revealPercentage(skillCover, skillPercentage);
         }, 10);
     } else {
-        spinnerRight.style.opacity = "0.0";
-        spinnerRight.style.transitionProperty = "opacity, transform";
-        spinnerRight.style.transitionDuration = "0s, 250ms";
-        spinnerLeft.style.transition = "250ms";
-
-        spinnerLeft.style.transitionTimingFunction = "ease-in";
+        spinnerRight.style.opacity = '0.0';
+        spinnerRight.style.transitionProperty = 'opacity, transform';
+        spinnerRight.style.transitionDuration = '0s, 250ms';
+        spinnerLeft.style.transition = '250ms';
+        spinnerLeft.style.transitionTimingFunction = 'ease-in';
 
         setTimeout(function() {
-            spinnerLeft.style.transform = "rotate(180deg)";
+            spinnerLeft.style.transform = 'rotate(180deg)';
             setTimeout(function() {
-                spinnerRight.style.opacity = "1.0";
+                spinnerRight.style.opacity = '1.0';
                 spinnerRight.style.transform = `rotate(${rotateDegree - 180}deg)`;
             }, 250);
-            setTimeout(() => skillCover.style.opacity = "0.4", 500);
+            revealPercentage(skillCover, skillPercentage);
         }, 10);
-
     }
+}
 
+function revealPercentage(skillCover, skillPercentage) {
+    setTimeout(function() {
+        skillCover.style.opacity = '0.4';
+        skillPercentage.style.opacity = '1.0';
+    }, 500);
 }
