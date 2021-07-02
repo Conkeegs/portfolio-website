f799e295df102df9c103d0eb7f7bd9af6ed3441b
Conkeegs <conkeegs@gmail.com>
2021-07-02 15:25:59 -0400

    fixed z-index problem with burger on 'index.html'

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,109 +5,81 @@ Scripts for 'index.html'
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 */
-let topBurger = document.getElementById("top-burger");
-let sideDrawer = document.getElementById("drawer");
-let cover = document.getElementById("cover");
+let topBurger = document.getElementById('top-burger');
+let sideDrawer = document.getElementById('drawer');
+let cover = document.getElementById('cover');
 let drawerOpened = false;
 
-topBurger.addEventListener("click", function () {
-
+topBurger.addEventListener('click', function() {
     if (drawerOpened) {
-
         closeDrawer();
-
-    }
-    else {
-
+    } else {
         openDrawer();
-
     }
 
     rotateBurger();
-
 });
 
 /**
  * This rotates the burger icon 90 degrees depending on if the drawer is opened or closed.
  */
 function rotateBurger() {
-
-    let topBurgerContainer = document.getElementById("index-top-burger-container");
-    topBurgerContainer.style.transition = "230ms";
+    let topBurgerContainer = document.getElementById(
+        'index-top-burger-container'
+    );
+    topBurgerContainer.style.transition = '230ms';
 
     if (drawerOpened) {
-
         setTimeout(function() {
-
-            topBurgerContainer.style.left = "55vw";
-            topBurgerContainer.style.transform = "rotate(90deg)";
-            topBurgerContainer.style.transition = "215ms";
-
-        },250);
-
-    }
-    else {
-
+            topBurgerContainer.style.left = '55vw';
+            topBurgerContainer.style.transform = 'rotate(90deg)';
+            topBurgerContainer.style.transition = '215ms';
+        }, 250);
+    } else {
         setTimeout(function() {
-
             topBurgerContainer.style.left = null;
-            topBurgerContainer.style.transform = "rotate(0)";
-            topBurgerContainer.style.transition = "150ms";
-
-        },250);
-
+            topBurgerContainer.style.transform = 'rotate(0)';
+            topBurgerContainer.style.transition = '150ms';
+        }, 250);
     }
-    
 }
 
 /**
  * Opens side drawer when burger icon is clicked.
  */
 function openDrawer() {
-
     drawerOpened = true;
 
-    cover.style.display = "block";
+    cover.style.display = 'block';
     setTimeout(function() {
-
-        cover.style.opacity = ".5";
-        cover.style.transition = "200ms";
-
+        cover.style.opacity = '.5';
+        cover.style.transition = '200ms';
     }, 0);
 
-    sideDrawer.style.display = "block";
+    sideDrawer.style.display = 'block';
     setTimeout(function() {
-
-        sideDrawer.style.right = "10vw";
-        sideDrawer.style.opacity = "1";
-        sideDrawer.style.transition = "215ms";
-
+        sideDrawer.style.right = '10vw';
+        sideDrawer.style.opacity = '1';
+        sideDrawer.style.transition = '215ms';
     }, 250);
-
 }
 
 /**
  * Closes side drawer when burger icon is clicked again.
  */
- function closeDrawer() {
-
+function closeDrawer() {
     drawerOpened = false;
 
     setTimeout(function() {
-
-        cover.style.opacity = "0";
-        cover.style.transition = "200ms";
-
+        cover.style.opacity = '0';
+        cover.style.transition = '200ms';
     }, 10);
-    cover.style.display = "block";
+    cover.style.display = 'block';
 
     setTimeout(function() {
-
-        sideDrawer.style.right = "80vw";
-        sideDrawer.style.opacity = "0";
-        sideDrawer.style.transition = "150ms";
-
+        sideDrawer.style.right = '80vw';
+        sideDrawer.style.opacity = '0';
+        sideDrawer.style.transition = '150ms';
     }, 250);
-    sideDrawer.style.display = "block";
-
+    sideDrawer.style.display = 'block';
 }
