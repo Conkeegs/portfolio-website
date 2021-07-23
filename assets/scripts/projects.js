7c27e6fe63b0296b625311309f197f3b1928405a
Conkeegs <conkeegs@gmail.com>
2021-07-23 03:17:38 -0400

    elements now fade away besides the project the user clicks on in 'projects.html'

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -17,18 +17,44 @@ for (let i = 0; i < projectContainers.length; i++) {
         setTimeout(function() {
             projectContainers[i].style.right = '1vw';
             projectContainers[i].style.bottom = '1vw';
-            openProject();
+            openProject(projectContainers[i]);
         }, 120);
     });
 }
 
-function openProject() {
-    setTimeout(toggleElementsDisplay, 100);
+/**
+ * This function simply takes in the project element the user clicked on and
+ * passes it to the 'toggleElementsDisplay' function.
+ *
+ * @param {element} element The project that was clicked on.
+ */
+function openProject(element) {
+    setTimeout(toggleElementsDisplay(element), 100);
 }
 
-function toggleElementsDisplay() {
+/**
+ * This function makes all of the elements, except the 'project' element the user clicked on,
+ * fade away. After 500ms, the elements that faded away's 'display' styles are set to
+ * 'none'.
+ *
+ * @param {element} element The project that was clicked on.
+ */
+function toggleElementsDisplay(element) {
     let top = document.getElementById('top');
 
     top.style.opacity = '0';
-    setTimeout(() => (top.style.display = 'none'), 500);
+    for (let i = 0; i < projectContainers.length; i++) {
+        if (!projectContainers[i].isEqualNode(element)) {
+            projectContainers[i].parentNode.style.opacity = '0';
+        }
+    }
+
+    setTimeout(function() {
+        top.style.display = 'none';
+        for (let i = 0; i < projectContainers.length; i++) {
+            if (!projectContainers[i].isEqualNode(element)) {
+                projectContainers[i].parentNode.style.display = 'none';
+            }
+        }
+    }, 500);
 }
